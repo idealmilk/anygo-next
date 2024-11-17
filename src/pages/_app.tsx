b173c3371c0be52a832c981e3d59679f14e14ee3
Matthew Gilligan <mcgilligan97@gmail.com>
2024-11-17 21:57:02 +0900

    Update footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
+import { Open_Sans } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+import '@/styles/globals.css'
+ 
+const openSans = Open_Sans({ subsets: [ 'latin' ]})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={openSans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
